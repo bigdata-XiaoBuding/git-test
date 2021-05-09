af64002cf771e779a5480286d8c4e6b83daba116
zk <1987790715@qq.com>
2021-05-09 16:05:05 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @Author zk
+ * @Date 2021/5/9 15:50
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
